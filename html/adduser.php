e517fed202aabc546beb0c55f8fe6540718cb547
Cyril de Bourgues <cyrildebourgues@gmail.com>
2019-01-07 15:40:14 +0100

    adduser.php done

--- html/adduser.php
+++ html/adduser.php
@@ -1,7 +1,7 @@
 <?php
 include_once 'header.php';
 if (!isset($_SESSION['admin']) or $_SESSION['admin'] != 1) {
-    header("Location: index.php");
+    exit(header("Location: index.php"));
 }
 ?>
 <body>  
@@ -16,82 +16,94 @@ if (!isset($_SESSION['admin']) or $_SESSION['admin'] != 1) {
 $passwordErr = $usernameErr = "";
 $newusername = $password = "";
 $enable = $admin = 0;
-$error = False;
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (empty($_POST["newusername"])) {
-        $usernameErr = "Username is required";
-        $error = True;
-    } else {
-        $newusername = $_POST["newusername"];
-    }
-    if (empty($_POST["password"])) {
-        $passwordErr = "Password is required";
-        $error = True;
-    } else {
+$valid = false;
+
+// Check if is a valid request (4 fileds expected)
+if ($_SERVER["REQUEST_METHOD"] == "POST" AND
+    isset($_POST["newusername"]) AND isset($_POST["password"]) OR
+    isset($_POST["enable"]) AND isset($_POST["admin"]) OR
+    !is_null($_POST["newusername"]) AND !is_null($_POST["password"])) {
+
+    // Check field types
+    if (filter_var($_POST["newusername"], PARAM_STR) !== false AND
+        filter_var($_POST["password"], PARAM_STR) !== false AND
+        filter_var($_POST["enable"], PARAM_INT) !== false AND
+        filter_var($_POST["admin"], PARAM_INT) !== false) {
+
+        $username = $_POST["username"];
         $password = $_POST["password"];
-    }
-    if ($_POST["enable"] == 1) {
+        $hash = \Sodium\crypto_pwhash_str($password,
+            \Sodium\CRYPTO_PWHASH_OPSLIMIT_INTERACTIVE,
+            \Sodium\CRYPTO_PWHASH_MEMLIMIT_INTERACTIVE);
+
         $enable = $_POST["enable"];
-    } else {
-        $enable = 0;
-    }
-    if ($_POST["admin"] == 1) {
         $admin = $_POST["admin"];
-    } else {
-        $admin = 0;
+        $valid = true;
     }
-    // check if user doesn't exist
-    if (!$error) {
-        try {
-            // Connect DB
-            $file_db = new PDO('sqlite:/usr/share/nginx/databases/database.sqlite');
-            // Set errormode to exceptions
-            $file_db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            // Select all users/password in DB
-            $file_db->exec("SELECT username FROM users;");
-            $exist = False;
-            foreach ($result as $row) {
-                if ($newusername == $row['username']) {
-                    $exist = True;
-                }
-            }
-            if (!$exist) {
-                $file_db->exec("INSERT INTO users (username, password, enable, admin) 
-					VALUES ('$newusername' , '$password', '$enable' ,'$admin');");
-                echo 'USER ADDED' . $admin . $enable;
-            }
-			else {
-				echo 'USER already exists';
-			}
-            $file_db = null;
+    else {
+        $errorMsg = "Invalid value";
+    }
+} else {
+    $errorMsg = "Bad request";
+}
+
+if ($valid) {
+    try {
+        // Connect DB
+        $file_db = new PDO('sqlite:/usr/share/nginx/databases/database.sqlite');
+
+        // Set errormode to exceptions
+        $file_db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        // Select all users/password in DB
+        $stmt = $file_db->prepare("SELECT username FROM users WHERE username=:username;");
+        $stmt->bindParam(':username', $username, PDO::PARAM_STR);
+        $file_db->execute();
+        $rowCount = $stmt->rowCount();
+
+        if ($rowCount == 0) {
+            $stmt = $file_db->prepare("INSERT INTO users (username, password, enable, admin) 
+					VALUES (:username, :password, :enable, :admin);");
+            $stmt->bindParam(':username', $username, PDO::PARAM_STR);
+            $stmt->bindParam(':password', $hash, PDO::PARAM_STR);
+            $stmt->bindParam(':enable', $enable, PDO::PARAM_INT);
+            $stmt->bindParam(':admin', $admin, PDO::PARAM_INT);
+            $file_db->execute();
+
+            echo 'USER ADDED' . $admin . $enable;
+        } else {
+            $errorMsg = 'this user already exists';
         }
-        catch(PDOException $e) {
-            // Print PDOException message
-            echo $e->getMessage();
-        }
+
+        // Proper DB connection close
+        $stmt->closeCursor();
+        $file_db = null;
+    }
+    catch(PDOException $e) {
+        // Print PDOException message
+        echo $e->getMessage();
     }
 }
+
 ?>
 
 <br><br>
+
+<?php if (!empty($errorMsg)) { ?>
+    <div>
+        <p><?php echo $errorMsg; ?></p>
+    </div>
+<?php } ?>
+
+<br>
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">  
 	Username: </br><input type="text" name="newusername">
-	<?php
-if (!empty($usernameErr)) {
-    echo $usernameErr;
-}
-?>
 	<br><br>
 	Password: </br><input type="text" name="password">
-	<?php
-if (!empty($passwordErr)) {
-    echo $passwordErr;
-}
-?>
 	<br><br>
-	Enable: </br><input type="checkbox" name="enable" value="1">
+	Enable: </br><input type="checkbox" name="enable" />
 	<br><br>
-	Admin: </br><input type="checkbox" name="admin" value="1">
+	Admin: </br><input type="checkbox" name="admin" />
 	<br><br>
 
 	<button type="submit">Submit</button>
